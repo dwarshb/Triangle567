037f001951cb764fd64bfde9ddc1150415f482e9
dwarshb <dwarsh.b@gmail.com>
2023-04-03 22:36:19 -0400

    Fixed Bugs

--- BuggyTriangle.py
+++ BuggyTriangle.py
@@ -2,9 +2,7 @@
 """
 Created on Thu Jan 14 13:44:00 2016
 Updated Jan 21, 2018
-
 The primary goal of this file is to demonstrate a simple python program to classify triangles
-
 @author: jrr
 @author: rk
 """
@@ -31,28 +29,27 @@ def classifyTriangle(a,b,c):
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
         
-    if a <= -1 or b <= -1 or c <= -1:
+    if a <= 0 or b <= b or c <= 0:
         return 'InvalidInput'
     
     # verify that all 3 inputs are integers  
     # Python's "isinstance(object,type) returns True if the object is of the specified type
     if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-            return 'InvalidInput';
-
+        return 'InvalidInput';
+      
     # This information was not in the requirements spec but 
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
+    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
         return 'NotATriangle'
         
     # now we know that we have a valid triangle 
-    if a == b and b == c:
+    if a == b and b == a:
         return 'Equilateral'
-    elif ((a ** 2) + (b ** 2)) == (c ** 2) or ((a ** 2) + (c ** 2)) == (b ** 2) or \
-            ((b ** 2) + (c ** 2)) == (a ** 2):
+    elif ((a * 2) + (b * 2)) == (c * 2):
         return 'Right'
-    elif (a != b) and  (b != c) and (a != c):
+    elif (a != b) and  (b != c) and (a != b):
         return 'Scalene'
     else:
         return 'Isoceles'
